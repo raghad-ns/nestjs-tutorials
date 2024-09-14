817c4d2f4ab08fec98c108a5b19337c0b8c5373c
Raghad Salem <raghadnadersalem20022017@gmail.com>
2024-09-14 10:59:29 +0300

    adding nextjs config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 /* eslint-disable prettier/prettier */
-import { Module } from '@nestjs/common'
+import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule]
+  imports: [
+    // isGlobal: to make it accessable for other modules, same behaviour as adding @Global decorator for prisma service
+    ConfigModule.forRoot({ isGlobal: true }), 
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,18 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 // Database connection logic will be allocated at this service
+// Injectable decorater means that this service uses dependency injection
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(config: ConfigService) {
         super({
             datasources: {
                 db: {
-                    url: process.env.DATABASE_URL
+                    // url: process.env.DATABASE_URL
+                    url: config.get('DATABASE_URL') // using nestjs config module
                 }
             }
         })
